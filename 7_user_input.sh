07a41cc6effc48da9560b274539c736e993d310c
ataullahkhanrifat <ataullahkhan.rifat@gmail.com>
2023-09-24 19:41:45 +0100

    User can Input

---
+++ 7_user_input.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#With this Scripts user can Input But in real world project In DevOps we mostly avoid User interection with system.
+echo "Enter your skill:"
+read SKILL
+
+echo " Your $SKILL skill is in high Demand in the IT industry."
+
+read -p 'Username: ' USR
+read -sp 'Password: ' pass
+
+echo
+
+echo "Login Successfull: Welcome USER $USR,"
