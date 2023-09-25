df750150cf700c479f4c6e7ed343a86e9b50651f
ataullahkhanrifat <ataullahkhan.rifat@gmail.com>
2023-09-25 12:40:38 +0100

    test Quiz

---
+++ 13_test_code.sh
@@ -0,0 +1,16 @@
+#echo "Bash version ${BASH_VERSION}"
+#for i in {0..10..2}
+#    echo "welcome $i times"
+#done
+
+
+
+
+
+#!/bin/bash
+echo "Bash version ${BASH_VERSION}"
+for i in {0..10..2}
+do
+    echo "welcome $i times"
+
+done
